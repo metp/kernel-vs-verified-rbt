0e545eca738f1085fb2e0b7f61aa87e2c3f0764a
Mete Polat <mete.polat@cs.tum.edu>
2021-08-08 17:46:38 +0200

    Remove unmodified RBT delete formalization

--- RBT.thy
+++ RBT.thy
@@ -1,4 +1,5 @@
-(* Author: Tobias Nipkow *)
+(* Author: Tobias Nipkow
+   Author: Mete Polat *)
 
 section \<open>Red-Black Trees\<close>
 
@@ -35,31 +36,5 @@ fun baliR :: "'a rbt \<Rightarrow> 'a \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt"
 fun paint :: "color \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
 "paint c Leaf = Leaf" |
 "paint c (Node l (a,_) r) = Node l (a,c) r"
-
-fun baldL :: "'a rbt \<Rightarrow> 'a \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
-"baldL (R t1 a t2) b t3 = R (B t1 a t2) b t3" |
-"baldL t1 a (B t2 b t3) = baliR t1 a (R t2 b t3)" |
-"baldL t1 a (R (B t2 b t3) c t4) = R (B t1 a t2) b (baliR t3 c (paint Red t4))" |
-"baldL t1 a t2 = R t1 a t2"
-
-fun baldR :: "'a rbt \<Rightarrow> 'a \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
-"baldR t1 a (R t2 b t3) = R t1 a (B t2 b t3)" |
-"baldR (B t1 a t2) b t3 = baliL (R t1 a t2) b t3" |
-"baldR (R t1 a (B t2 b t3)) c t4 = R (baliL (paint Red t1) a t2) b (B t3 c t4)" |
-"baldR t1 a t2 = R t1 a t2"
-
-fun join :: "'a rbt \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
-"join Leaf t = t" |
-"join t Leaf = t" |
-"join (R t1 a t2) (R t3 c t4) =
-  (case join t2 t3 of
-     R u2 b u3 \<Rightarrow> (R (R t1 a u2) b (R u3 c t4)) |
-     t23 \<Rightarrow> R t1 a (R t23 c t4))" |
-"join (B t1 a t2) (B t3 c t4) =
-  (case join t2 t3 of
-     R u2 b u3 \<Rightarrow> R (B t1 a u2) b (B u3 c t4) |
-     t23 \<Rightarrow> baldL t1 a (B t23 c t4))" |
-"join t1 (R t2 a t3) = R (join t1 t2) a t3" |
-"join (R t1 a t2) t3 = R t1 a (join t2 t3)" 
 
 end
--- RBT_Set.thy
+++ RBT_Set.thy
@@ -33,20 +33,6 @@ fun color :: "'a rbt \<Rightarrow> color" where
 "color Leaf = Black" |
 "color (Node _ (_, c) _) = c"
 
-fun del :: "'a::linorder \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
-"del x Leaf = Leaf" |
-"del x (Node l (a, _) r) =
-  (case cmp x a of
-     LT \<Rightarrow> if l \<noteq> Leaf \<and> color l = Black
-           then baldL (del x l) a r else R (del x l) a r |
-     GT \<Rightarrow> if r \<noteq> Leaf\<and> color r = Black
-           then baldR l a (del x r) else R l a (del x r) |
-     EQ \<Rightarrow> join l r)"
-
-definition delete :: "'a::linorder \<Rightarrow> 'a rbt \<Rightarrow> 'a rbt" where
-"delete x t = paint Black (del x t)"
-
-
 subsection "Functional Correctness Proofs"
 
 lemma inorder_paint: "inorder(paint c t) = inorder t"
@@ -69,30 +55,6 @@ lemma inorder_insert:
   "sorted(inorder t) \<Longrightarrow> inorder(insert x t) = ins_list x (inorder t)"
 by (simp add: insert_def inorder_ins inorder_paint)
 
-lemma inorder_baldL:
-  "inorder(baldL l a r) = inorder l @ a # inorder r"
-by(cases "(l,a,r)" rule: baldL.cases)
-  (auto simp:  inorder_baliL inorder_baliR inorder_paint)
-
-lemma inorder_baldR:
-  "inorder(baldR l a r) = inorder l @ a # inorder r"
-by(cases "(l,a,r)" rule: baldR.cases)
-  (auto simp:  inorder_baliL inorder_baliR inorder_paint)
-
-lemma inorder_join:
-  "inorder(join l r) = inorder l @ inorder r"
-by(induction l r rule: join.induct)
-  (auto simp: inorder_baldL inorder_baldR split: tree.split color.split)
-
-lemma inorder_del:
- "sorted(inorder t) \<Longrightarrow>  inorder(del x t) = del_list x (inorder t)"
-by(induction x t rule: del.induct)
-  (auto simp: del_list_simps inorder_join inorder_baldL inorder_baldR)
-
-lemma inorder_delete:
-  "sorted(inorder t) \<Longrightarrow> inorder(delete x t) = del_list x (inorder t)"
-by (auto simp: delete_def inorder_del inorder_paint)
-
 
 subsection \<open>Structural invariants\<close>
 
@@ -100,6 +62,7 @@ lemma neq_Black[simp]: "(c \<noteq> Black) = (c = Red)"
 by (cases c) auto
 
 text\<open>The proofs are due to Markus Reiter and Alexander Krauss.\<close>
+text\<open>The Linux RBT insert proof modification is due to Mete Polat.\<close>
 
 fun bheight :: "'a rbt \<Rightarrow> nat" where
 "bheight Leaf = 0" |
@@ -202,89 +165,6 @@ by (induct x t rule: ins.induct) (auto simp: inv_baliL inv_baliR invc2I)
 theorem rbt_insert2: "rbt t \<Longrightarrow> rbt (insert x t)"
   by (simp add: inv_ins color_paint_Black invh_paint rbt_def insert_def invc3I ins_Red_invcs)
 
-subsubsection \<open>Deletion\<close>
-
-lemma bheight_paint_Red:
-  "color t = Black \<Longrightarrow> bheight (paint Red t) = bheight t - 1"
-by (cases t) auto
-
-lemma invh_baldL_invc:
-  "\<lbrakk> invh l;  invh r;  bheight l + 1 = bheight r;  invc r \<rbrakk>
-   \<Longrightarrow> invh (baldL l a r) \<and> bheight (baldL l a r) = bheight r"
-by (induct l a r rule: baldL.induct)
-   (auto simp: invh_baliR invh_paint bheight_baliR bheight_paint_Red)
-
-lemma invh_baldL_Black: 
-  "\<lbrakk> invh l;  invh r;  bheight l + 1 = bheight r;  color r = Black \<rbrakk>
-   \<Longrightarrow> invh (baldL l a r) \<and> bheight (baldL l a r) = bheight r"
-by (induct l a r rule: baldL.induct) (auto simp add: invh_baliR bheight_baliR) 
-
-lemma invc_baldL: "\<lbrakk>invc2 l; invc r; color r = Black\<rbrakk> \<Longrightarrow> invc (baldL l a r)"
-by (induct l a r rule: baldL.induct) (simp_all add: invc_baliR)
-
-lemma invc2_baldL: "\<lbrakk> invc2 l; invc r \<rbrakk> \<Longrightarrow> invc2 (baldL l a r)"
-by (induct l a r rule: baldL.induct) (auto simp: invc_baliR paint2 invc2I)
-
-lemma invh_baldR_invc:
-  "\<lbrakk> invh l;  invh r;  bheight l = bheight r + 1;  invc l \<rbrakk>
-  \<Longrightarrow> invh (baldR l a r) \<and> bheight (baldR l a r) = bheight l"
-by(induct l a r rule: baldR.induct)
-  (auto simp: invh_baliL bheight_baliL invh_paint bheight_paint_Red)
-
-lemma invc_baldR: "\<lbrakk>invc l; invc2 r; color l = Black\<rbrakk> \<Longrightarrow> invc (baldR l a r)"
-by (induct l a r rule: baldR.induct) (simp_all add: invc_baliL)
-
-lemma invc2_baldR: "\<lbrakk> invc l; invc2 r \<rbrakk> \<Longrightarrow>invc2 (baldR l a r)"
-by (induct l a r rule: baldR.induct) (auto simp: invc_baliL paint2 invc2I)
-
-lemma invh_join:
-  "\<lbrakk> invh l; invh r; bheight l = bheight r \<rbrakk>
-  \<Longrightarrow> invh (join l r) \<and> bheight (join l r) = bheight l"
-by (induct l r rule: join.induct) 
-   (auto simp: invh_baldL_Black split: tree.splits color.splits)
-
-lemma invc_join: 
-  "\<lbrakk> invc l; invc r \<rbrakk> \<Longrightarrow>
-  (color l = Black \<and> color r = Black \<longrightarrow> invc (join l r)) \<and> invc2 (join l r)"
-by (induct l r rule: join.induct)
-   (auto simp: invc_baldL invc2I split: tree.splits color.splits)
-
-text \<open>All in one:\<close>
-
-lemma inv_baldL:
-  "\<lbrakk> invh l;  invh r;  bheight l + 1 = bheight r; invc2 l; invc r \<rbrakk>
-   \<Longrightarrow> invh (baldL l a r) \<and> bheight (baldL l a r) = bheight r
-  \<and> invc2 (baldL l a r) \<and> (color r = Black \<longrightarrow> invc (baldL l a r))"
-by (induct l a r rule: baldL.induct)
-   (auto simp: inv_baliR invh_paint bheight_baliR bheight_paint_Red paint2 invc2I)
-
-lemma inv_baldR:
-  "\<lbrakk> invh l;  invh r;  bheight l = bheight r + 1; invc l; invc2 r \<rbrakk>
-   \<Longrightarrow> invh (baldR l a r) \<and> bheight (baldR l a r) = bheight l
-  \<and> invc2 (baldR l a r) \<and> (color l = Black \<longrightarrow> invc (baldR l a r))"
-by (induct l a r rule: baldR.induct)
-   (auto simp: inv_baliL invh_paint bheight_baliL bheight_paint_Red paint2 invc2I)
-
-lemma inv_join:
-  "\<lbrakk> invh l; invh r; bheight l = bheight r; invc l; invc r \<rbrakk>
-  \<Longrightarrow> invh (join l r) \<and> bheight (join l r) = bheight l
-  \<and> invc2 (join l r) \<and> (color l = Black \<and> color r = Black \<longrightarrow> invc (join l r))"
-by (induct l r rule: join.induct) 
-   (auto simp: invh_baldL_Black inv_baldL invc2I split: tree.splits color.splits)
-
-lemma neq_LeafD: "t \<noteq> Leaf \<Longrightarrow> \<exists>l x c r. t = Node l (x,c) r"
-by(cases t rule: tree2_cases) auto
-
-lemma inv_del: "\<lbrakk> invh t; invc t \<rbrakk> \<Longrightarrow>
-   invh (del x t) \<and>
-   (color t = Red \<longrightarrow> bheight (del x t) = bheight t \<and> invc (del x t)) \<and>
-   (color t = Black \<longrightarrow> bheight (del x t) = bheight t - 1 \<and> invc2 (del x t))"
-by(induct x t rule: del.induct)
-  (auto simp: inv_baldL inv_baldR inv_join dest!: neq_LeafD)
-
-theorem rbt_delete: "rbt t \<Longrightarrow> rbt (delete x t)"
-by (metis delete_def rbt_def color_paint_Black inv_del invh_paint)
-
 text \<open>Overall correctness:\<close>
 
 interpretation S: Set_by_Ordered
@@ -297,13 +177,15 @@ next
 next
   case 3 thus ?case by(simp add: inorder_insert)
 next
-  case 4 thus ?case by(simp add: inorder_delete)
+  (* Linux RBT delete not formalized *)
+  case 4 thus ?case sorry
 next
   case 5 thus ?case by (simp add: rbt_def empty_def) 
 next
   case 6 thus ?case by (simp add: rbt_insert) 
 next
-  case 7 thus ?case by (simp add: rbt_delete) 
+  (* Linux RBT delete not formalized *)
+  case 7 thus ?case sorry
 qed
 
 
